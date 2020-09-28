708a52270db508a7c866c30b744af62a4880cbad
mkosir <kosir.mar@gmail.com>
2020-09-28 12:51:19 +0200

    update scripts

--- scripts/be-node-dev.sh
+++ scripts/be-node-dev.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
+echo "Install bash and execute 'wait-for-it.sh' script"
+apk add --update bash
 ./scripts/wait-for-it.sh $PG_HOST:5432 --timeout=30 --strict -- echo "postgres up and running"
+
 npm run migration:run
 npm run seed:run
 npm run dev
