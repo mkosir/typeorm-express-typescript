13b33738d48f3172189580a89ac5415422042881
mkosir <kosir.mar@gmail.com>
2020-09-28 13:16:43 +0200

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,8 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-empty-interface': 0,
 
     // Import
     'import/order': [
@@ -31,7 +33,16 @@ module.exports = {
   },
 
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
   },
 };
