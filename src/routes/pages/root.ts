917bfcc03c78392321e01f1bb4df0da2a4c1ac75
mkosir <kosir.mar@gmail.com>
2020-10-21 12:18:50 +0200

    update response

--- src/routes/pages/root.ts
+++ src/routes/pages/root.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res, next) => {
-  res.status(200).header('Content-Type', 'text/html').send(`<h4>RESTful API boilerplate</h4>`);
+  res.status(200).header('Content-Type', 'text/html').send(`<h4>💊 RESTful API boilerplate</h4>`);
 });
 
 export default router;
