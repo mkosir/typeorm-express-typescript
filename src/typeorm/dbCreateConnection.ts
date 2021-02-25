1e262374d4967ad2728c72e0b7b22fdd389e388c
mkosir <kosir.mar@gmail.com>
2021-02-25 14:31:47 +0100

    refactor: update routes

--- src/typeorm/dbCreateConnection.ts
+++ src/typeorm/dbCreateConnection.ts
@@ -1,12 +1,13 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection } from 'typeorm';
 
 import config from './config/ormconfig';
 
-export const dbCreateConnection = async () => {
+export const dbCreateConnection = async (): Promise<Connection | null> => {
   try {
     const conn = await createConnection(config);
     console.log(`Database connection success. Connection name: '${conn.name}' Database: '${conn.options.database}'`);
   } catch (err) {
     console.log(err);
   }
+  return null;
 };
