4099e6380d5e0e76dcf68eebbca307554c767503
mkosir <kosir.mar@gmail.com>
2022-02-24 11:29:48 +0100

    refactor: typeorm naming

--- src/orm/config/ormconfig.ts
+++ src/orm/config/ormconfig.ts
@@ -3,6 +3,7 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 const config: ConnectionOptions = {
   type: 'postgres',
+  name: 'default',
   host: process.env.PG_HOST,
   port: Number(process.env.PG_PORT),
   username: process.env.POSTGRES_USER,
--- src/orm/dbCreateConnection.ts
+++ src/orm/dbCreateConnection.ts
@@ -1,4 +1,4 @@
-import { Connection, createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionManager } from 'typeorm';
 
 import config from './config/ormconfig';
 
@@ -7,6 +7,10 @@ export const dbCreateConnection = async (): Promise<Connection | null> => {
     const conn = await createConnection(config);
     console.log(`Database connection success. Connection name: '${conn.name}' Database: '${conn.options.database}'`);
   } catch (err) {
+    if (err.name === 'AlreadyHasActiveConnectionError') {
+      const activeConnection = getConnectionManager().get(config.name);
+      return activeConnection;
+    }
     console.log(err);
   }
   return null;
